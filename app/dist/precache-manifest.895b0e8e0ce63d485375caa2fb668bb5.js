531d2b2c0884f73ce2d7d12c26c4e43e51894595
aarti-git <im.aarti.suryawanshi@gmail.com>
2021-06-19 23:54:23 +0530

    enable dist

---
+++ app/dist/precache-manifest.895b0e8e0ce63d485375caa2fb668bb5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781917cf554ca5c6cd6f",
+    "url": "/css/app.a61f60a3.css"
+  },
+  {
+    "revision": "c8331c5baf69d5b563bf",
+    "url": "/css/chunk-2bf05644.eb013463.css"
+  },
+  {
+    "revision": "0942dd0ec937c4625c5e",
+    "url": "/css/chunk-40983e7e.362844ca.css"
+  },
+  {
+    "revision": "99ee197903ac55e8f96c",
+    "url": "/css/chunk-a0e9928a.37a50e6c.css"
+  },
+  {
+    "revision": "e79aeea8136717e5b46487d266172225",
+    "url": "/img/aarti.e79aeea8.jpg"
+  },
+  {
+    "revision": "e79aeea8136717e5b46487d266172225",
+    "url": "/img/aarti.jpg"
+  },
+  {
+    "revision": "5421e707bffc027ddecd5ffd35f66aa5",
+    "url": "/img/yogesh.5421e707.jpg"
+  },
+  {
+    "revision": "5421e707bffc027ddecd5ffd35f66aa5",
+    "url": "/img/yogesh.jpg"
+  },
+  {
+    "revision": "0381f647c9f55f549f55491bf53aaee6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781917cf554ca5c6cd6f",
+    "url": "/js/app.c577d6d7.js"
+  },
+  {
+    "revision": "c8331c5baf69d5b563bf",
+    "url": "/js/chunk-2bf05644.9423dc67.js"
+  },
+  {
+    "revision": "0942dd0ec937c4625c5e",
+    "url": "/js/chunk-40983e7e.1b563922.js"
+  },
+  {
+    "revision": "99ee197903ac55e8f96c",
+    "url": "/js/chunk-a0e9928a.d6e00538.js"
+  },
+  {
+    "revision": "6e95d39ddf4b697c3dff",
+    "url": "/js/chunk-vendors.93b39217.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
